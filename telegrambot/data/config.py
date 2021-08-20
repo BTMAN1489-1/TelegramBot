53ad32109795015bfc212650b418b56a1a1e7fca
BTMAN1489-1 <79748440+BTMAN1489-1@users.noreply.github.com>
2021-08-20 13:25:26 +1000

    Update config.py

--- telegrambot/data/config.py
+++ telegrambot/data/config.py
@@ -7,7 +7,8 @@ API_TOKEN = env.str("API_TOKEN")
 WEBAPP_HOST = '0.0.0.0'
 WEBAPP_PORT = env.int("PORT", 5000)
 WEBHOOK_HOST = env.str("WEBHOOK_HOST", None)
-WEBHOOK_PATH = f"/webhook/{API_TOKEN}"
-WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
+URL_SAFE_TOKEN = base64.standard_b64encode(API_TOKEN.encode("utf-8")).decode("utf-8")
+WEBHOOK_PATH = f"/webhook/{URL_SAFE_TOKEN}"
+WEBHOOK_URL = f"https://{WEBHOOK_HOST}{WEBHOOK_PATH}"
 ADMINS = env.list("ADMINS", [])
 
